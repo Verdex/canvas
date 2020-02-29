e47109e91e0ed3002c679b3a651021f48dcc8ef8
David L. Inman <davidlevere@gmail.com>
2020-02-29 14:18:32 -0500

    setting up server tcp listener stuffs

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,11 @@
+
+use std::net::{TcpListener, TcpStream};
+
 fn main() {
-    println!("Hello, world!");
+
+    let maybe_listener = TcpListener::bind("127.0.0.1:3000");
+    match maybe_listener {
+        Err(e) => println!("failed to bind {}", e),
+        Ok(listener) => println!("Bound successfuly"),
+    }
 }
