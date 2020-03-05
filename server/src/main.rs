16feb7cecd1e3aa724b9369ec23241dd5047fd40
David L. Inman <davidlevere@gmail.com>
2020-03-04 23:15:39 -0500

    thread and channel notes

--- server/src/main.rs
+++ server/src/main.rs
@@ -3,6 +3,26 @@ mod login;
 
 fn main() -> std::io::Result<()> {
 
+/*
+    use std::thread;
+    let child = thread::spawn( move || ... );
+
+    let result = child.join(); 
+
+
+    use std::sync::mpsc;
+
+    let (tx, rx) = mpsc::channel();
+    tx.send(val).unwrap();
+
+    rx.try_recv() // returns result<t,e> .  Error when there are no messages
+    rx.recv() // blocks
+
+
+    mpsc::Sender::clone(&tx)
+*/
+
+
     let zz = "\n";
     let bz = zz.as_bytes();
     println!("!!!!!!!!!!!!!!!");
