b0ebb7bad9d859df48cf092d0873bd820dd9fb3c
David L. Inman <davidlevere@gmail.com>
2020-03-06 12:27:12 -0500

    created parser functionality

--- server/src/packet_parser.rs
+++ server/src/packet_parser.rs
@@ -1,4 +1,42 @@
 
+use std::str::Chars;
+use std::iter::Peekable;
+
+pub enum Packet {
+
+}
+
+
 // [<packet_type>|atom:<param>|list:<length>:<param1>:<param2>]
 
+struct Parser<'a> {
+    orig : Peekable<Chars<'a>>,
+    done : bool
+}
 
+impl<'a> Parser<'a> {
+    fn is(&mut self, c : char) -> Result<(), String> {
+        match self.orig.peek() {
+            Some(value) if *value == c => Ok(()),
+            Some(value) =>  Err(format!("Expected {} but found {}", c, *value)),
+            None => Err(format!("Expected {} but found Nothing", c)),
+        }
+    }
+
+    fn next(&mut self) {
+        match self.orig.next() {
+            None => self.done = true,
+            _ => (),
+        }
+    }
+}
+
+
+pub fn parse( packet : &str ) -> Result<Packet, String> {
+    let mut parser = Parser{ orig : packet.chars().peekable(), done : false };
+
+    parser.is( '[' )?;
+    parser.next();
+     
+     Err("Unknown Failure".to_string())
+}
